d8cfc5ebf9ad98df19b71a5ba1dd0c3d58568a71
Saket Shah <saket.shah@saketsh-ltmdy83.internal.salesforce.com>
2022-02-10 15:40:39 +0530

    77277 Document Upload

--- salesforce_sfdx/main/default/lwc/cCIUploadDocumentQuickAction/cCIUploadDocumentQuickAction.js
+++ salesforce_sfdx/main/default/lwc/cCIUploadDocumentQuickAction/cCIUploadDocumentQuickAction.js
@@ -4,11 +4,9 @@ export default class CCIUploadDocumentQuickAction extends LightningElement {
 		
 		@api recordId;
     retrievedRecordId = false;
-		prefill={};
-		
+		prefill={};	
 		renderedCallback() {
 				if (!this.retrievedRecordId && this.recordId) {
-
 						this.retrievedRecordId = true; // Escape case from recursion
 						var data = '\{"ContextId":"'+this.recordId+'"}';
 						this.prefill = data;
