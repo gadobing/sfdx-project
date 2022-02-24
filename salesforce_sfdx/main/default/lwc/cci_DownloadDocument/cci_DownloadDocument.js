13d414c46a55457b5b730848e6cdd051112aa9e9
Saket Shah <saket.shah@saketsh-ltmdy83.internal.salesforce.com>
2022-02-24 11:20:17 +0530

    Account Member Page

--- salesforce_sfdx/main/default/lwc/cci_DownloadDocument/cci_DownloadDocument.js
+++ salesforce_sfdx/main/default/lwc/cci_DownloadDocument/cci_DownloadDocument.js
@@ -1,26 +1,36 @@
 import { LightningElement ,api, wire, track } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
+import { NavigationMixin } from 'lightning/navigation';
 import downloadjs from '@salesforce/resourceUrl/downloadjs';
+import { CloseActionScreenEvent } from 'lightning/actions';
 import getDocumentDetails from '@salesforce/apex/CCI_DownloadDocumentController.getDownloadDocumentDetails';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class PrintJobsContainer extends LightningElement {
-    boolShowSpinner = false;
+export default class Cc_DownloadDocument extends NavigationMixin(LightningElement){
+    boolShowSpinner = true;
     pdfString;
     @api recordId;
-    
     downloadFile(){ 
         this.boolShowSpinner = true;
         getDocumentDetails({ docSFId: this.recordId })
         .then(result => {
             console.log(result);
             this.boolShowSpinner = false;
-
+            if(result.sfDocId !='')
+            {
+                this.navigateToFiles(result.sfDocId);
+                this.closeQuickAction();
+                return;
+            }
             if(result.statusCode == 200 ){  
-                console.log('@@result',result);
-                var strFile = "data:"+result.mimeType+";base64,"+result.base64String;
-                console.log('@@strFile',strFile);
-                window.download(strFile, "Document", result.mimeType);
+              //  console.log('@@result',result);//
+                //var strFile = "data:"+result.mimeType+";base64,"+result.base64String;
+               
+                var strFile = "data:application/octet-stream"+";base64,"+result.base64String;
+                var docName = result.docName =='' ? 'Document':result.docName;
+                var docNamewExtension =  result.extension ==''?docName :docName+'.'+result.extension;
+ 
+                window.download(strFile, docNamewExtension, "application/octet-stream");
                 }
             else if(result.statusCode == 404){
                 const evt = new ShowToastEvent({
@@ -41,7 +51,9 @@ export default class PrintJobsContainer extends LightningElement {
                     });
                 this.dispatchEvent(evt);
                 }
+                this.closeQuickAction();
         }).catch(error => {
+            console.log('@@Error: ');
             console.log('Error: ' +error);
 
             const evt = new ShowToastEvent({
@@ -51,11 +63,34 @@ export default class PrintJobsContainer extends LightningElement {
                 mode: 'sticky'
             });
             this.dispatchEvent(evt);
+            this.boolShowSpinner = false;
+            this.closeQuickAction();
         });
     }
     renderedCallback() {
         loadScript(this, downloadjs)
         .then(() => console.log('Loaded download.js'))
         .catch(error => console.log(error));
-    }        
+    }    
+    connectedCallback() {
+        setTimeout(() => {
+            this.downloadFile();
+        }, 1000);
+       
+    }
+    navigateToFiles(recordIds) {
+        this[NavigationMixin.Navigate]({
+          type: 'standard__namedPage',
+          attributes: {
+              pageName: 'filePreview'
+          },
+          state : {
+              recordIds: recordIds
+              
+          }
+        })
+      }
+      closeQuickAction() {
+        this.dispatchEvent(new CloseActionScreenEvent());
+    }  
 }
