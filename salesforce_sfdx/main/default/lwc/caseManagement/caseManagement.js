2e5c69027177a097e43cc439ecc501dd475a8335
Aniket Bhalerao <abhalerao@salesforce.com>
2022-03-07 12:48:11 +0530

    US-81942 -Aniket

--- salesforce_sfdx/main/default/lwc/caseManagement/caseManagement.js
+++ salesforce_sfdx/main/default/lwc/caseManagement/caseManagement.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class CaseManagement extends LightningElement {}
+export default class CaseManagement extends LightningElement {
+    @api recordId;
+    get prefill(){
+       return '{"ContextId":"'+this.recordId+'"}';
+    }
+}
