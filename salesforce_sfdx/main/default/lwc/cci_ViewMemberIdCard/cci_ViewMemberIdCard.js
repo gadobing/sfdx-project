356516becd4e74f6baa9e737e37744d4240bf79d
Aniket Bhalerao <abhalerao@salesforce.com>
2022-04-05 11:56:48 +0530

    view member id card updates

--- salesforce_sfdx/main/default/lwc/cci_ViewMemberIdCard/cci_ViewMemberIdCard.js
+++ salesforce_sfdx/main/default/lwc/cci_ViewMemberIdCard/cci_ViewMemberIdCard.js
@@ -1,23 +1,71 @@
 import { LightningElement ,api, wire, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import getMemberCardDetails from '@salesforce/apex/CCI_ViewMemberIdCardController.getMemberCardDetails';
+//import getMemberCardDetails from '@salesforce/apex/CCI_ViewMemberIdCardController.getMemberCardDetails';
+import { OmniscriptActionCommonUtil } from 'omnistudio/omniscriptActionUtils';
+
 export default class Cci_ViewMemberIdCard extends LightningElement {   
+
     cardDetails;
     strData;
     strData2;
     error;
     customMessage ='';
     showLoading = true;
+    errorMessage = '';
+    isError = false;
     @api recordId;
     @track isModalOpen = false;
-    handleClick(event) {
+
+    connectedCallback() {
+        setTimeout(() => {
+          //  this.invoiceDetails();
+          this._actionUtilClass = new OmniscriptActionCommonUtil();
+          console.log("setTimeout");
+          this.triggerRemote();
+        }, 1000);
+    }
+    triggerRemote() {
+        console.log("triggerRemote");
+        const input = {
+            memberId: this.recordId
+        };
+ 
+        const params = {
+
+            input: JSON.stringify(input),
+            sClassName: 'CCI_ViewMemberIdCardController',
+            sMethodName: 'getMemberCardDetails',
+            options: '{}',
+        };
+    
+        this._actionUtilClass
+            .executeAction(params, null, this, null, null)
+            .then(response => {
+                this.boolShowSpinner = false;
+                window.console.log(response);
+                 var result = response.result;
+                 window.console.log(JSON.parse(result.data));
+                if(result.data){
+                    this.handleData(JSON.parse(result.data));
+                }
+                if(result.Error){
+                 this.error = result.Error;
+                    console.log('error',error);
+
+                    
+                    this.errorMessage = this.error;
+                    this.isError = true;
+                    this.isModalOpen = false;
+                }
+            });
+        }
+    handleData(result) {
         this.isModalOpen = true;
         if(this.cardDetails === undefined || (this.cardDetails != undefined && this.cardDetails.statusCode !=200)){
             this.showLoading =true;
            // console.log('@@getMemberCardDetails');//12972630W05t
            // console.log('@@recordId',this.recordId);//0017j00000ghZNHAA2
-            getMemberCardDetails({ memberId: this.recordId })
-                .then(result => {
+            
                     
                     this.showLoading =false;
                     console.log('result',result);
@@ -32,39 +80,21 @@ export default class Cci_ViewMemberIdCard extends LightningElement {
                 
                     }
                     else if(result.statusCode == 404){
-                        const evt = new ShowToastEvent({
-                            title: 'Error',
-                            message: 'Member ID card not found for this member',
-                            variant: 'error',
-                            mode: 'sticky'
-                        });
-                        this.dispatchEvent(evt);
+                        
+                        this.errorMessage = 'Member ID card not found for this member';
+                        this.isError = true;
                         this.isModalOpen = false;
                     }
                     else{
-                        const evt = new ShowToastEvent({
-                            title: 'Error',
-                            message: 'Something went wrong, please try again after some time(Error Code= '+result.statusCode+',Message = '+result.status+')!',
-                            variant: 'error',
-                            mode: 'sticky'
-                        });
-                        this.dispatchEvent(evt);
+                        
+                        this.errorMessage = 'Something went wrong, please try again after some time(Error Code= '+result.statusCode+',Message = '+result.status+')!';
+                        this.isError = true;
                         this.isModalOpen = false;
                     }
-                })
-                .catch(error => {
-                    this.error = error;
-                    console.log('error',error);
-
-                    const evt = new ShowToastEvent({
-                        title: 'Error',
-                        message: error.body.message,
-                        variant: 'error',
-                        mode: 'sticky'
-                    });
-                    this.dispatchEvent(evt);
-                    this.isModalOpen = false;
-                });
+               
+               
+                    
+               
         }    
 
     }
@@ -75,5 +105,6 @@ export default class Cci_ViewMemberIdCard extends LightningElement {
     closeModal() {
         this.isModalOpen = false;
     }
+
 
 }
